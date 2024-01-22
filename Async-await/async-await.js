408debd6d62c695792d644f7e5eb880edf6750e9
Blacklion567 <naviedaj567@gmail.com>
2024-01-22 18:37:17 +0800

    Async-await

--- Async-await/async-await.js
+++ Async-await/async-await.js
@@ -13,9 +13,36 @@ function getAsyncData() {
 
 function getData() {
   return new Promise(function (resolve) {
-    getAsyncData().then(function (data) {
+// function getAsyncData() {
+//   // return a new Promise object
+//   return new Promise(function (resolve) {
+//     // set a timeout of 2 seconds
+//     setTimeout(function () {
+//       // resolve the Promise object with an object containing the name, age and gender
+//       resolve({
+//         name: 'Blacklion567',
+//         age: 21,
+//         gender: 'Male'
+//       });
+//     }, 2000);
+//   });
+// }
+
+// function getData() {
+//   // return a new Promise object
+//   return new Promise(function (resolve) {
+//     // call the getAsyncData() function and then call the resolve function with the data returned from getAsyncData()
+//     getAsyncData().then(function (data) {
+//       resolve(data);
+//     });
+//   });
+// }
+
+// // call the getAsyncData() function and then call the console.log function with the data returned from getAsyncData()
+getAsyncData().then(function (data) {
       resolve(data);
-    });
+  console.log(data);
+});
   });
 }
 
